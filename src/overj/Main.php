b0c8ab589f7e05d417ab43aa3432611310115269
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-23 16:06:49 +0200

    Create Main.php

---
+++ src/overj/Main.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+namespace overj;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+
+class function onEnable(){
+$this->getLogger()->info("[0v3rCore] Join activé");
+}
+
+class function onJoin(PlayerJoinEvent $event){
+
+	$player = $event->getPlayer();
+	$event->setJoinMessage( joinMessage: "");
+
+	if(!$player->hasPlayedBefore()){
+
+	Server::getInstance()->broadcastMessage( message: "§7[§6+§7] §7Bienvenu(e) a ce joueur§6" . $player->getName() . " qui se connecte pour la premiere fois.§7");
+
+	}else{
+	
+	Server::getInstance()->broadcastMessage( message: "§7[§a+§7]" . $player->getName() . " ");
+	
+}
+
+}
+
+class function onQuit(PlayerQuitEvent $event){
+
+	$player = $event->getPlayer();
+	$event->setQuitMessage( quitMessage: "");
+}
+
+}
+
